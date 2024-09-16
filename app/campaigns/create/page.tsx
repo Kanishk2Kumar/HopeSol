25565577e231dcba77afeb450673aa6412b7c9fa
Harsh Saindane <harshwardhansaindane@gmail.com>
2024-09-16 14:54:59 +0530

    Added create campaign route

---
+++ app/campaigns/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="text-3xl text-slate-950 font-bold underline">
+      Create Campaign
+    </div>
+  );
+}
+
+export default Page;
