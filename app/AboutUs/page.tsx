29287f7f42eb14f9df0bc3f7272712e9c32afabb
Kanishk <kanishkkumar127@gmail.com>
2024-09-18 22:19:58 +0530

    Added WhySol

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { About } from '@/components/About'
 import { Authors } from '@/components/Authors'
+import { WhySol } from '@/components/WhySol'
 const page = () => {
   return (
     <div>
         <section>
             <About />
+        </section>
+        <section>
+            <WhySol />
         </section>
         <section>
             <Authors />
