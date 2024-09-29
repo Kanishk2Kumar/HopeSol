961464401562d0b8c20090365a76cda43784abef
Kanishk <kanishkkumar127@gmail.com>
2024-09-29 16:10:34 +0530

    Removed Dummy Data

--- public/icons/index.js
+++ public/icons/index.js
@@ -1,7 +1,8 @@
 import search from "./search.svg";
 import copyrightSign from "./copyright-sign.svg";
 import linkedin from "./linkedin.svg";
+import video from "./video.svg";
 
 export default {
-    search, copyrightSign, linkedin
+    search, copyrightSign, linkedin, video
 }
